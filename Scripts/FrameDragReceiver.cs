fe66a5b9fe7b336926409d89458340842acdecf0
Oznogon <garrett@oznogon.com>
2018-02-09 21:07:34 -0800

    Add scripts.

---
+++ Scripts/FrameDragReceiver.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+// Frame dragging method by why485/Brian Hernandez
+// https://www.youtube.com/watch?v=1VFXQXjmdIk
+
+namespace Oznogon.FrameDrag
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class FrameDragReceiver : MonoBehaviour
+    {
+        public Vector3 lastDragAngularVelocity;
+        public Vector3 lastDragVelocity;
+        public FrameDragger dragger;
+        public Rigidbody draggerRb;
+        public bool useFixedUpdate = true;
+
+        private void OnTriggerEnter(Collider collision)
+        {
+            dragger = collision.transform.GetComponent<FrameDragger>();
+            if (dragger != null)
+            {
+                draggerRb = dragger.GetComponent<Rigidbody>();
+                Debug.Log(name + ": dragger.name = " + dragger.name
+                    + "\ndraggerRb.GetPointVelocity(transform.position) = " + draggerRb.GetPointVelocity(transform.position)
+                    + "\ndraggerRb.angularVelocity * Mathf.Rad2Deg = " + (draggerRb.angularVelocity * Mathf.Rad2Deg)
+                );
+            }
+        }
+
+        private void OnTriggerExit(Collider collision)
+        {
+            dragger = null;
+        }
+
+        private void Update()
+        {
+            if (!useFixedUpdate)
+                GetDragged();
+        }
+
+        private void FixedUpdate()
+        {
+            if (useFixedUpdate)
+                GetDragged();
+        }
+
+        void GetDragged()
+        {
+            if (dragger != null)
+            {
+                lastDragVelocity = draggerRb.GetPointVelocity(transform.position);
+                lastDragAngularVelocity = draggerRb.angularVelocity * Mathf.Rad2Deg;
+                transform.position += lastDragVelocity * Time.deltaTime;
+                transform.Rotate(lastDragAngularVelocity * Time.deltaTime, Space.World);
+            }
+        }
+    }
+}
---
+++ Scripts/FrameDragger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+// Frame dragging method by why485/Brian Hernandez
+// https://www.youtube.com/watch?v=1VFXQXjmdIk
+
+namespace Oznogon.FrameDrag
+{
+    [RequireComponent(typeof(Rigidbody))]
+
+    public class FrameDragger : MonoBehaviour
+    {
+        public Vector3 carrierVelocity;
+
+        private void FixedUpdate()
+        {
+            carrierVelocity = transform.GetComponent<Rigidbody>().velocity;
+        }
+    }
+}
