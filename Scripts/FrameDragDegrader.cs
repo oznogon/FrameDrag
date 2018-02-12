d0aebba424bd0cba9b0604da62e48a93465fdaef
Oznogon <garrett@oznogon.com>
2018-02-11 21:00:34 -0800

    Move drag degrader into its own script.

---
+++ Scripts/FrameDragDegrader.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+namespace Oznogon.FrameDrag
+{
+    [RequireComponent(typeof(FrameDragger))]
+    public class FrameDragDegrader : MonoBehaviour
+    {
+        private FrameDragger dragger;
+
+        [Tooltip("Whether the effect is the same everywhere in the trigger " +
+            "collider, or if it degrades linearly outside of the effect " +
+            "radius.")]
+        public bool degradeByDistance = true;
+
+        [Tooltip("The radius of maximum effect, if it doesn't degrade over " +
+            "distance from the effect origin. Has no effect if Degrade By " +
+            "Distance is disabled.")]
+        public float dragEffectRadius = 10.0f;
+
+        [Tooltip("The origin of the effect if it degrades over distance. If " +
+            "left empty on start, this automatically uses this object's " +
+            "transform. Has no effect if Degrade By Distance is disabled.")]
+        public Transform dragEffectOrigin;
+
+        private void Start()
+        {
+            // If there's no effect origin set, use this object's transform.
+            if (dragEffectOrigin == null)
+            {
+                dragEffectOrigin = transform;
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            // If you don't plan on using degredation over distance, you can
+            // remove OnTriggerStay entirely and lighten the load.
+            dragger = GetComponent<FrameDragger>();
+            FrameDragReceiver receiver = other.GetComponent<FrameDragReceiver>();
+
+            if (receiver != null && receiver.isActiveAndEnabled)
+            {
+                if (degradeByDistance)
+                {
+                    // A full dragMagnitude (1.0) means the receiver almost
+                    // perfectly matches the dragger's rotation and velocity.
+                    // The magnitude should be full if the receiver is within
+                    // the dragEffectRadius of the dragEffectOrigin.position,
+                    // and degrade the further the object is from that radius.
+                    //
+                    // There are many ways to do this, and all of them should
+                    // be smarter than this.
+                    float receiverDistance = Vector3.Distance(receiver.transform.position, dragEffectOrigin.position);
+                    receiver.dragMagnitude = Mathf.Clamp01(dragEffectRadius / receiverDistance);
+                }
+                else
+                {
+                    receiver.dragMagnitude = 1.0f;
+                }
+            }
+        }
+    }
+}
--- Scripts/FrameDragger.cs
+++ Scripts/FrameDragger.cs
@@ -13,29 +13,6 @@ namespace Oznogon.FrameDrag
         [Tooltip("List of objects being dragged. Updated automatically.")]
         public List<FrameDragReceiver> dragged;
 
-        [Tooltip("Whether the effect is the same everywhere in the trigger " +
-            "collider, or if it degrades linearly outside of the effect " +
-            "radius.")]
-        public bool degradeEffectByDistance = true;
-
-        [Tooltip("The radius of maxium effect, if it doesn't degrade over " +
-            "distance from the effect origin.")]
-        public float dragEffectRadius = 3.0f;
-
-        [Tooltip("The origin of the effect if it degrades over distance. If " +
-            "left empty on start, this automatically uses this object's " +
-            "transform.")]
-        public Transform effectOrigin;
-
-        private void Start()
-        {
-            // If there's no effect origin set, use this object's transform.
-            if (effectOrigin == null)
-            {
-                effectOrigin = transform;
-            }
-        }
-
         private void OnTriggerEnter(Collider other)
         {
             FrameDragReceiver receiver = other.GetComponent<FrameDragReceiver>();
@@ -80,34 +57,6 @@ namespace Oznogon.FrameDrag
             }
         }
 
-        private void OnTriggerStay(Collider other)
-        {
-            // If you don't plan on using degredation over distance, you can
-            // remove OnTriggerStay entirely and lighten the load.
-            FrameDragReceiver receiver = other.GetComponent<FrameDragReceiver>();
-
-            if (receiver != null && receiver.isActiveAndEnabled)
-            {
-                if (degradeEffectByDistance)
-                {
-                    // A full dragMagnitude (1.0) means the receiver almost
-                    // perfectly matches the dragger's rotation and velocity.
-                    // The magnitude should be full if the receiver is within
-                    // the dragEffectRadius of the effectOrigin.position, and
-                    // degrade more the further the object is from that radius.
-                    //
-                    // There are many ways to do this, all of them smarter than
-                    // mine.
-                    float receiverDistance = Vector3.Distance(receiver.transform.position, effectOrigin.position);
-                    receiver.dragMagnitude = Mathf.Clamp01(dragEffectRadius / receiverDistance);
-                }
-                else
-                {
-                    receiver.dragMagnitude = 1.0f;
-                }
-            }
-        }
-
         private void OnDisable()
         {
             // We have to track all dragged objects because trigger colliders
@@ -117,7 +66,7 @@ namespace Oznogon.FrameDrag
             // object wasn't disabled. :( Thanks, Unity!
             if (dragged != null)
             {
-                // I probably shouldn't use foreach.
+                // I shouldn't use foreach.
                 foreach (FrameDragReceiver receiver in dragged)
                 {
                     receiver.RemoveDrag();
